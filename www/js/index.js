d610117c87540d3a12aec7a6f1743667345d425b
Jonathan Carrasco Garcia <jonathancg90@gmail.com>
2013-09-09 23:47:50 -0500

    Add: exmaple acelelrometro

--- www/js/index.js
+++ www/js/index.js
@@ -39,19 +39,21 @@ var app = {
     getConnectionInfo: function(parentElement) {
 
         var connectionTypeElement = $('#conn-type');
-
-        var networkState = navigator.connection.type;
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'Wifi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.CELL]     = 'Cellular connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        connectionTypeElement.html(states[networkState]);
+        var estado = "No reconocido";
+        if(navigator.connection != undefined){
+            var networkState = navigator.connection.type;
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'Wifi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cellular connection';
+            states[Connection.NONE]     = 'No network connection';
+            estado = states[networkState];
+        }
+        connectionTypeElement.html(estado);
     }
 };
 
