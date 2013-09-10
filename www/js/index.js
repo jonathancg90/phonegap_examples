2cff7c6779dc3de9d09e53b0f4f22eabfaa0aac5
Jonathan Carrasco Garcia <jonathancg90@gmail.com>
2013-09-09 23:14:53 -0500

    Add: exmaple conection

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,57 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
+// Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+//Esperando que el dispositivo este listo para usar
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+//Dispositivo listo para usar ... ejecutar eventos ...
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+//Ejecucion de eventos
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var parentElement = $(id);
+        app.getDeviceInfo(parentElement);
+        app.getConnectionInfo(parentElement);
 
-        console.log('Received Event: ' + id);
+    },
+
+//========================================
+
+    getDeviceInfo: function(parentElement) {
+        var modelElement         = $('#model');
+        var versionElement       = $('#version');
+        var platformElement      = $('#platform');
+        var uuidElement          = $('#uuid');
+        var deviceVersionElement = $('#device-vesion');
+
+        modelElement.html(device.model);
+        versionElement.html(device.cordova);
+        platformElement.html(device.platform);
+        uuidElement.html(device.uuid);
+        deviceVersionElement.html(device.version);
+    },
+    getConnectionInfo: function(parentElement) {
+
+        var connectionTypeElement = $('#conn-type');
+
+        var networkState = navigator.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'Wifi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cellular connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        connectionTypeElement.html(states[networkState]);
     }
 };
+
